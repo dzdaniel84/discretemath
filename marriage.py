29ca7b467df9169731fbc15568ec2dfd574d0de2
dzdaniel84 <dzdaniel84@gmail.com>
2017-01-16 13:24:00 -0800

    Added print statements

--- marriage.py
+++ marriage.py
@@ -4,11 +4,13 @@ class Simulation:
 	
 	def __init__(self, n):
 		self.boys, self.girls = numbers(self, n), letters(self, n)
+		self.day = 0
 
 	def run(self):
 		while notOptimal(self):
 			[b.propose() for b in self.boys]
 			[g.consider() for g in self.girls]
+			self.day += 1
 
 	def rank(self, person):
 		if Type(person) == Boy:
@@ -31,9 +33,11 @@ class Boy:
 		self.wishlist = sim.rank(self);
 
 	def propose(self):
+		print("{} proposed to {}.".format(self, self.wishlist[0]))
 		self.wishlist[0].suitors.append(self)
 
 	def remove(self, other):
+		print("{} was removed from {}'s list.".format(other,, self))
 		if other == self.wishlist[0]
 			self.wishlist.pop(0)
 
@@ -47,13 +51,15 @@ class Girl:
 		self.suitors = []
 
 	def consider(self):
+		print("Current list of suitors for {}: {}".format(self.suitors, self))
 		if len(self.suitors) == 0:
+			print("No suitors yet for {}. Moving on.".format(self))
 			return
 		best_suitor = max([self.wishlist.index(s) for s in self.suitors])
 		for s in self.suitors:
 			if s != self.suitors[best_suitor]
 				s.remove(self)
-		print(self)
+		print("{} selects {} to move on to the next round.".format(self, self.suitors[best_suitor]))
 
 	def __repr__():
 		return self.name
@@ -65,4 +71,4 @@ def letters(sim, n):
 	return [Girl(sim, chr(i+96)) for i in range(1, n+1)]
 
 def notOptimal(sim):
-	pass
+	True
