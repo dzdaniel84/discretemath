c0181a16f1bdf048c13a6e0660d96181e422c54e
dzdaniel84 <dzdaniel84@gmail.com>
2017-01-17 00:03:15 -0800

    Added table visualisation; made runthrough a lot nicer

--- marriage.py
+++ marriage.py
@@ -3,18 +3,21 @@ import random
 class Simulation:
 	
 	def __init__(self, n):
+		self.n = n
 		self.boys, self.girls = numbers(self, n), letters(self, n)
 		[b.createList() for b in self.boys]
 		[g.createList() for g in self.girls]
 		self.day = 0
 
 	def run(self):
-		print(self.boys, self.girls)
+		self.table()
 		optimal = True
 		while optimal:
+			print("\n\n══ Day {} ══".format(self.day))
 			[b.propose() for b in self.boys]
-			optimal = all([g.consider() for g in self.girls])
+			optimal = not all([g.consider() for g in self.girls])
 			self.day += 1
+		print("\n\n")
 
 	def rank(self, person):
 		if type(person) == Boy:
@@ -31,6 +34,16 @@ class Simulation:
 			return self.girls[ord(char)-49]
 		return self.boys[ord(char)-97]
 
+	def table(self):
+		l = (self.n//10) + 1
+		print("╔═════╦╦══" + "══╦══".join('═'*l for i in range(self.n)) + "══╗\t\t" + \
+			"╔═════╦╦══" + "══╦══".join('═'*l for i in range(self.n)) + "══╗")
+		for i in range(self.n):
+			print("║  {}  ║║".format(self.boys[i]) + "║".join("  {}  ".format(g.__repr__()) for g in self.boys[i].wishlist) + "║" \
+			+ "\t\t" + "║  {}  ║║".format(self.girls[i]) + "║".join("  {}  ".format(b.__repr__()) for b in self.girls[i].wishlist) + "║")
+		print("╚═════╩╩══" + "══╩══".join('═'*l for i in range(self.n)) + "══╝\t\t" + \
+			"╚═════╩╩══" + "══╩══".join('═'*l for i in range(self.n)) + "══╝")
+
 class Boy:
 
 	def __init__(self, sim, name):
@@ -44,11 +57,12 @@ class Boy:
 		self.wishlist[0].suitors.append(self)
 
 	def remove(self, other):
-		print("{} was removed from {}'s list.".format(other, self))
+		print("{} rejects {}.".format(other, self))
 		if other == self.wishlist[0]:
 			self.wishlist.pop(0)
 
 	def __repr__(self):
+		#return " "*(((self.sim.n//10) + 1)-(int(self.name)//10)) + str(self.name)
 		return self.name
 
 class Girl:
@@ -61,13 +75,15 @@ class Girl:
 		self.wishlist = self.sim.rank(self);
 
 	def consider(self):
-		print("Current list of suitors for {}: {}".format(self, self.suitors))
 		if len(self.suitors) == 0:
 			print("No suitors yet for {}. Moving on.".format(self))
 			return False
 		best_suitor = max(self.suitors, key = lambda suitor: self.wishlist.index(suitor)) #can simplify more
-		self.suitors = [best_suitors]
-		print("{} selects {} to move on to the next round.".format(self, best_suitor))
+		for suitor in self.suitors:
+			if suitor is not best_suitor:
+				suitor.remove(self)
+		self.suitors = [best_suitor]
+		print("{} tells {} 'Maybe'.".format(self, best_suitor))
 		return True
 
 	def __repr__(self):
@@ -79,4 +95,4 @@ def numbers(sim, n):
 def letters(sim, n):
 	return [Girl(sim, chr(i+96)) for i in range(1, n+1)]
 
-Simulation(3).run()
+Simulation(9).run()
